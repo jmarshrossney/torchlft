8f4e014446b00bf3319f124e598c4e0f3bd42d5a
marshrossney <17361029+marshrossney@users.noreply.github.com>
2022-05-09 23:30:31 +0100

    add tox, flake8, coverage

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -13,6 +13,7 @@ import torchlft.metrics
 
 SEED = 8967452301
 
+
 @pytest.fixture
 def metrics():
     random.seed(SEED)
@@ -20,11 +21,12 @@ def metrics():
     log_weights = torch.rand(10000)
     return torchlft.metrics.LogWeightMetrics(log_weights)
 
+
 def test_metrics(metrics):
     assert math.isclose(metrics.kl_divergence, -0.49898, abs_tol=1e-4)
     assert math.isclose(metrics.acceptance, 0.83268, abs_tol=1e-4)
-    assert math.isclose(metrics.integrated_autocorrelation, 0.72413, abs_tol=1e-4)
+    assert math.isclose(
+        metrics.integrated_autocorrelation, 0.72413, abs_tol=1e-4
+    )
     assert math.isclose(metrics.effective_sample_size, 0.92328, abs_tol=1e-4)
     assert metrics.longest_rejection_run == 53
-
-
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ import torch
 
 import torchlft.utils
 
+
 def test_nn_kernel():
     expected = torch.Tensor([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
     got = torchlft.utils.nearest_neighbour_kernel(lattice_dim=2)
