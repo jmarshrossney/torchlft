3faa53cf153df6b767dafa84152803828ea05f98
marshrossney <17361029+marshrossney@users.noreply.github.com>
2022-05-09 22:11:55 +0100

    copy over metrics and utils from previous project

---
+++ src/torchlft/metrics.py
@@ -0,0 +1,157 @@
+from __future__ import annotations
+
+import math
+from random import random
+
+import torch
+
+Tensor = torch.Tensor
+
+
+class LogWeightMetrics:
+    r"""Lightweight metrics computed using the log-weights of a model output.
+
+    'Log-weight' refers to the logarithm of the statistical weight of a
+    generated configuration, :math:`\Psi`, defined
+
+    .. math::
+
+        \log w(\Psi) = \log p(\Psi) - \log q(\Psi)
+
+    where :math:`p(\Psi)` and :math:`q(\Psi)` are the target and model
+    probability densities, respectively.
+
+    Parameters
+    ----------
+    log_weights
+        One-dimensional tensor of log-weights
+    """
+
+    def __init__(self, log_weights: Tensor) -> None:
+        assert (
+            log_weights.dim() == 1
+        ), "log_weights should be a 1-dimensional tensor"
+        self._log_weights = log_weights
+        self._run_metropolis_hastings()
+
+    def _run_metropolis_hastings(self) -> None:
+        weights = self._log_weights.tolist()
+        curr_weight = weights.pop(0)
+        history = []
+
+        for prop_weight in weights:
+            if random() < min(1, math.exp(curr_weight - prop_weight)):
+                curr_weight = prop_weight
+                history.append(1)
+            else:
+                history.append(0)
+
+        self._history = history
+
+    @property
+    def kl_divergence(self) -> float:
+        r"""Kullbach-Leibler divergence of the sample.
+
+        .. math::
+
+            D_{KL} = \frac{1}{N} \sum_{\{\Phi\}} - \log w(\Psi)
+        """
+        return float(self._log_weights.neg().mean())
+
+    @property
+    def acceptance(self) -> float:
+        r"""Fraction of proposals accepted during Metropolis-Hastings.
+
+        Given target probability density :math:`p(\phi)`, candidate
+        configurations :math:`\{ \phi \sim \tilde{p}(\phi) \}` are
+        accepted with a probability
+
+        .. math::
+
+            A(\phi \to \phi^\prime) = \min \left( 1,
+            \frac{\tilde{p}(\phi)}{p(\phi)}
+            \frac{p(\phi^\prime)}{\tilde{p}(\phi^\prime)} \right) \, .
+
+        Generally speaking, the acceptance rate will be larger if the overlap
+        between the model and the target densities is larger.
+        """
+        return sum(self._history) / len(self._history)
+
+    @property
+    def longest_rejection_run(self) -> int:
+        r"""Largest number of consecutive rejections in the sampling phase."""
+        n = 0
+        n_max = 0
+        for step in self._history:
+            if step:
+                n += 1
+                if n > n_max:
+                    n_max = n
+            else:
+                n = 0
+        return n_max
+
+    @property
+    def integrated_autocorrelation(self) -> float:
+        r"""Integrated autocorrelation derived from the accept/reject history.
+
+        In the limit :math:`t \to \infty` the autocorrelation function can be
+        re-interpreted as the probability of failing to transition over a given
+        number of Markov chain steps.
+
+        .. math::
+
+            \frac{\Gamma(t)}{\Gamma(0)} =
+            \Pr(t \text{ consecutive rejections} )
+
+        An estimate of this probability is obtained by simply summing the
+        number of occurrences of a :math:`t` consecutive rejections, and
+        normalizing by the largest number of occurrences that are possible;
+        that is, by :math:`T - t + 1` where :math:`T` is the total number
+        of Markov chain steps.
+
+        References: https://arxiv.org/abs/1904.12072
+        """
+        tally = [0 for _ in range(len(self._history))]  # first element is t=1
+        n_rej = 0
+
+        for step in self._history:
+            if step:  # candidate accepted
+                if n_rej > 0:
+                    for t in range(n_rej):
+                        tally[t] += n_rej - t
+                n_rej = 0
+            else:  # candidate rejected
+                n_rej += 1
+
+        for t in range(n_rej):  # catch last run
+            tally[t] += n_rej - t
+
+        # Normalize
+        autocorr = [
+            a / b for a, b in zip(tally, range(len(self._history), 0, -1))
+        ]
+
+        return 0.5 + sum(autocorr)
+
+    @property
+    def effective_sample_size(self) -> float:
+        r"""Effective sample size normalised by the size of the sample.
+
+        .. math::
+
+            N_{eff} = \frac{
+                \left[ \frac{1}{N} \sum_{\{\Phi\}} w(\Phi) \right]^2
+                }{
+                \frac{1}{N} \sum_{\{\Phi\}} w(\Phi)^2
+            }
+
+        References
+        ----------
+        https://arxiv.org/abs/2101.08176
+        """
+        ess = torch.exp(
+            self._log_weights.logsumexp(dim=0).mul(2)
+            - self._log_weights.mul(2).logsumexp(dim=0)
+        )
+        return ess.div(len(self._log_weights))
---
+++ src/torchlft/utils.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import itertools
+
+import torch
+
+
+def make_checkerboard(lattice_shape: list[int]) -> torch.BoolTensor:
+    """Return a boolean mask that selects 'even' lattice sites."""
+    assert all(
+        [n % 2 == 0 for n in lattice_shape]
+    ), "each lattice dimension should be even"
+    checkerboard = torch.full(lattice_shape, False)
+    if len(lattice_shape) == 1:
+        checkerboard[::2] = True
+    elif len(lattice_shape) == 2:
+        checkerboard[::2, ::2] = True
+        checkerboard[1::2, 1::2] = True
+    else:
+        raise NotImplementedError("d > 2 currently not supported")
+    return checkerboard
+
+
+def laplacian_2d(lattice_length: int) -> torch.Tensor:
+    """Creates a 2d Laplacian matrix.
+
+    This works by taking the kronecker product of the one-dimensional
+    Laplacian matrix with the identity.
+
+    Notes
+    -----
+    For now, assume a square lattice. Periodic BCs are also assumed.
+    """
+    identity = torch.eye(lattice_length)
+    lapl_1d = (
+        2 * identity  # main diagonal
+        - torch.diag(torch.ones(lattice_length - 1), diagonal=1)  # upper
+        - torch.diag(torch.ones(lattice_length - 1), diagonal=-1)  # lower
+    )
+    lapl_1d[0, -1] = lapl_1d[-1, 0] = -1  # periodicity
+    lapl_2d = torch.kron(lapl_1d, identity) + torch.kron(identity, lapl_1d)
+    return lapl_2d
+
+
+def nearest_neighbour_kernel(lattice_dim) -> torch.Tensor:
+    identity_kernel = torch.zeros([3 for _ in range(lattice_dim)])
+    identity_kernel.view(-1)[pow(3, lattice_dim) // 2] = 1
+
+    nn_kernel = torch.zeros([3 for _ in range(lattice_dim)])
+    for shift, dim in itertools.product([+1, -1], range(lattice_dim)):
+        nn_kernel.add_(identity_kernel.roll(shift, dim))
+
+    return nn_kernel.view(1, 1, *nn_kernel.shape)
