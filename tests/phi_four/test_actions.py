bb898ca3196533f66bf973be878267770ee29675
marshrossney <17361029+marshrossney@users.noreply.github.com>
2022-07-12 02:06:50 +0100

    update actions, auto select parametrisation

--- tests/phi_four/test_actions.py
+++ tests/phi_four/test_actions.py
@@ -1,24 +1,22 @@
 import torch
 
 from torchlft.phi_four.actions import (
-    phi_four_action_standard,
-    phi_four_action_ising,
-    PhiFourActionStandard,
-    PhiFourActionIsing,
+    phi_four_action,
+    PhiFourAction,
 )
 
 
 def test_all_equal_no_quart():
     beta = 1
     m_sq = -2
-    lam = 0
+    lamda = 0
 
     x = torch.rand(100, 10, 10)
 
-    s_func = phi_four_action_standard(x, m_sq, lam)
-    i_func = phi_four_action_ising(x, beta, lam)
-    s_cls = PhiFourActionStandard(m_sq, lam)(x)
-    i_cls = PhiFourActionIsing(beta, lam)(x)
+    s_func = phi_four_action(x, m_sq=m_sq, lamda=lamda)
+    i_func = phi_four_action(x, beta=beta, lamda=lamda)
+    s_cls = PhiFourAction(m_sq=m_sq, lamda=lamda)(x)
+    i_cls = PhiFourAction(beta=beta, lamda=lamda)(x)
 
     assert torch.allclose(s_func, s_cls)
     assert torch.allclose(i_func, i_cls)
